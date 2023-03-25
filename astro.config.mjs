03fd15a5daf76045fbde232cb417e9e403184e73
Hamza Khalid <hamzakhalid829@gmail.com>
2023-03-25 13:54:35 +0500

    ssr??

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import image from "@astrojs/image";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [image()],
+  output: "server",
+  adapter: vercel()
 });
